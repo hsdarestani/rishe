ab084336cbaa6ca55f8f3c71200ccc5a3df2f60b
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-21 03:14:39 +0330

    ثبت صفحه مدیریت اتصال ووکامرس

--- src/Plugin.php
+++ src/Plugin.php
@@ -23,6 +23,7 @@ use Rishe\Procurement\Infrastructure\WordPress\ProcurementRestApi;
 use Rishe\Sales\Infrastructure\WordPress\SalesRestApi;
 use Rishe\Tax\Infrastructure\WordPress\TaxRestApi;
 use Rishe\Treasury\Infrastructure\WordPress\TreasuryRestApi;
+use Rishe\WooCommerce\Infrastructure\WordPress\WooCommerceSyncAdminPage;
 use Rishe\WooCommerce\Infrastructure\WordPress\WooCommerceSyncRestApi;
 use Rishe\WooCommerce\Infrastructure\WordPress\WooCommerceSyncRuntime;
 
@@ -52,6 +53,7 @@ final class Plugin
         (new AnalyticsRestApi())->register();
         (new OperationsRuntime())->register();
         (new AnalyticsRuntime())->register();
+        (new WooCommerceSyncAdminPage())->register();
         (new WooCommerceSyncRestApi())->register();
         (new WooCommerceSyncRuntime())->register();
         (new RisheCliRegistrar())->register();
