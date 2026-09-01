13e38cdfcfb91d1484dce7ee4f779d2bd4c7b50b
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-01 20:23:36 +0330

    chore: bump Rishe to 2.3.5

--- rishe.php
+++ rishe.php
@@ -4,7 +4,7 @@
  * Plugin Name: ریشه – سیستم عملیاتی کسب‌وکار
  * Plugin URI: https://github.com/hsdarestani/rishe
  * Description: سیستم یکپارچه ریشه برای ووکامرس، انبار، تأمین، کارتابل مالی، فروش آفلاین ایونت، لجستیک و B2B.
- * Version: 2.3.4
+ * Version: 2.3.5
  * Author: Hossein Darestani
  * Requires at least: 6.5
  * Requires PHP: 8.1
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('RISHE_VERSION', '2.3.4');
+define('RISHE_VERSION', '2.3.5');
 define('RISHE_DB_VERSION', '2026080102');
 define('RISHE_FILE', __FILE__);
 define('RISHE_PATH', plugin_dir_path(__FILE__));
